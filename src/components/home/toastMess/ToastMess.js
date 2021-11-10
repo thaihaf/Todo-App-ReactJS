c7485ff8e8a32555791df6f7027962fa392213a0
thaiha2268 <thaiha2268@gmail.com>
2021-11-10 16:47:49 +0700

    80% render frontend

--- src/components/home/toastMess/ToastMess.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './toastMess.css';
-export default function ToastMess() {
-    return (
-        <div className="ToastMess">
-            
-        </div>
-    )
-}
