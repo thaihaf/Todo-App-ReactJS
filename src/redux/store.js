83ce462c71329523f9600c2a59a1dccf4b6db144
thaiha2268 <thaiha2268@gmail.com>
2021-12-30 10:50:54 +0700

    handle extraReducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -13,11 +13,3 @@ const store = configureStore({
 });
 
 export default store;
-
-// import { createStore } from "redux";
-// import rootReducer from "./reducer";
-// import { composeWithDevTools } from "redux-devtools-extension";
-
-// const composedEnhancers = composeWithDevTools();
-// // const store = createStore(rootReducer, initValue, enhancers);
-// const store = createStore(rootReducer, composedEnhancers);
